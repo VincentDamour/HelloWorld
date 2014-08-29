ec55db757c3f13d37b205f06735b6099f4eea6e5
funny-vince <funny-vince@hotmail.com>
2014-08-29 12:33:42 -0400

    Commit initial
    Commit initial du laboratoire 1

---
+++ HelloWorld/Controllers/PageValidationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class PageValidationController : Controller
+    {
+        //
+        // GET: /PageValidation/
+
+        public ActionResult PageValidation()
+        {
+            ViewBag.Message = "Voici l'exercise de validation.";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PageValidation(ContactModels Model)
+        {
+            ViewBag.Message = "Voici l'exercise de validation.";
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Home/Index");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ HelloWorld/Models/ContactModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelloWorld.Models
+{
+    public class ContactModels
+    {
+        [Required]
+        public string Nom { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Commentaire { get; set; }
+    }
+}
